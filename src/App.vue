a02e4cf099a48e065f1306d3384224f5927c59c8
Lyric Wai <w@persper.org>
2017-10-01 21:31:12 +0800

    First commit.

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+html, body {
+  margin: 0;
+}
+body {
+  background: #333;
+}
+#app {
+  font-family: Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+textarea {
+  font-size: 14px;
+}
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="main">
+    <wireframe></wireframe>
+  </div>
+</template>
+
+<script>
+import Wireframe from '@/components/Wireframe'
+export default {
+  name: 'hello',
+  components: {
+    'wireframe': Wireframe
+  },
+  data () {
+    return {
+      msg: 'Main'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+
+.main {
+}
+</style>
