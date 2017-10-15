be87235547606dd5d1e84ec75d2c43ee579f47de
Jinglei Ren <jinglei@persper.org>
2017-10-15 16:55:26 +0800

    Add test for web API

--- test/test_flask_app.py
+++ test/test_flask_app.py
@@ -5,23 +5,27 @@ from pprint import pprint
 import requests
 
 
-def test_next_question(address):
-    url = address + '/survey/v1/projects/6f2295863057c2e7059b74a01c79ab707f8789c3/questions/next'
+def test_next_question(address, project_id):
+    url = '%s/survey/v1/projects/%s/questions/next' % (address, project_id)
     print('Next question: ' + url)
     headers = {'X-USR-TOKEN': 'tqxe2wmETskTsWq6t_MZwaUdzm8HY3Cqvahg-R-oR38'}
     r = requests.get(url, headers=headers)
     if r.ok:
         pprint(r.json())
+        if r.json()['status'] == 0:
+            return r.json()['data']['question']
+        else:
+            return None
     else:
         print(r.text)
 
 
-def test_submit_answer(address):
-    url = address + '/survey/v1/projects/6f2295863057c2e7059b74a01c79ab707f8789c3/questions/' \
-                    'a248f3aaf9c99bed17ef9ca24d131cebeaa1906f'
+def test_submit_answer(address, project_id, question_id, selected, reason):
+    url = '%s/survey/v1/projects/%s/questions/%s' % (
+        address, project_id, question_id)
     print('Submit answer: ' + url)
     headers = {'X-USR-TOKEN': 'tqxe2wmETskTsWq6t_MZwaUdzm8HY3Cqvahg-R-oR38'}
-    payload = {'selected': '915330ffc269eed821d652292993ff75b717a66b'}
+    payload = {'selected': selected, 'reason': reason}
     r = requests.post(url, headers=headers, json=payload)
     if r.ok:
         pprint(r.json())
@@ -29,29 +33,33 @@ def test_submit_answer(address):
         print(r.text)
 
 
-def test_next_review(address):
-    url = address + '/survey/v1/projects/6f2295863057c2e7059b74a01c79ab707f8789c3/reviews/next'
+def test_next_review(address, project_id):
+    url = '%s/survey/v1/projects/%s/reviews/next' % (address, project_id)
     print('Next review: ' + url)
     headers = {'X-USR-TOKEN': 'tqxe2wmETskTsWq6t_MZwaUdzm8HY3Cqvahg-R-oR38'}
     r = requests.get(url, headers=headers)
     if r.ok:
         pprint(r.json())
+        if r.json()['status'] == 0:
+            return r.json()['data']['review']
+        else:
+            return None
     else:
         print(r.text)
 
 
-def test_submit_review(address):
-    url = address + '/survey/v1/projects/6f2295863057c2e7059b74a01c79ab707f8789c3/reviews/' \
-                    'a248f3aaf9c99bed17ef9ca24d131cebeaa1906f'
+def test_submit_review(address, project_id, review_id, **kwargs):
+    url = '%s/survey/v1/projects/%s/reviews/%s' % (
+        address, project_id, review_id)
     print('Submit review: ' + url)
     headers = {'X-USR-TOKEN': 'tqxe2wmETskTsWq6t_MZwaUdzm8HY3Cqvahg-R-oR38'}
-    payload = {'reason': 'Who knows.',
-               'commitLabels': [
-                   {'commitID': 'b35414f93aa5caaff115791d4040271047df25b3',
-                    'labelID': '506f25f62a7e5acfb4b5f866a570e78e4efd638a'},
-                   {'commitID': 'b35414f93aa5caaff115791d4040271047df25b3',
-                    'labelName': '506f25f62a7e5acfb4b5f866a570e78e4efd638a'},
-               ]}
+    payload = {}
+    commit_labels = kwargs.get('commit_labels')
+    if commit_labels is not None:
+        payload['commitLabels'] = commit_labels
+    comment = kwargs.get('comment')
+    if comment is not None:
+        payload['comment'] = comment
     r = requests.post(url, headers=headers, json=payload)
     if r.ok:
         pprint(r.json())
@@ -59,40 +67,67 @@ def test_submit_review(address):
         print(r.text)
 
 
-def test_new_label(address):
-    url = address + '/survey/v1/projects/6f2295863057c2e7059b74a01c79ab707f8789c3/labels/new'
-    print('New label: ' + url)
-    headers = {'X-USR-TOKEN': 'tqxe2wmETskTsWq6t_MZwaUdzm8HY3Cqvahg-R-oR38'}
-    payload = {'labelTitle': 'tiny'}
-    r = requests.post(url, headers=headers, json=payload)
-    if r.ok:
-        pprint(r.json())
-    else:
-        print(r.text)
-
-
-def test_get_labels(address):
-    url = address + '/survey/v1/projects/6f2295863057c2e7059b74a01c79ab707f8789c3/labels'
+def test_get_labels(address, project_id):
+    url = '%s/survey/v1/projects/%s/labels' % (address, project_id)
     print('Get labels: ' + url)
     headers = {'X-USR-TOKEN': 'tqxe2wmETskTsWq6t_MZwaUdzm8HY3Cqvahg-R-oR38'}
     r = requests.get(url, headers=headers)
     if r.ok:
         pprint(r.json())
+        return r.json()['data']
     else:
         print(r.text)
 
 
 def main():
+    # The test of database fills the graph store with 6 pairs of commits.
+    # Also, it has answered and reviewed 2 pairs.
     parser = argparse.ArgumentParser(description='test app server')
     parser.add_argument('-a', '--address', default='http://api.persper.org',
                         help='RESTful web service address')
     args = parser.parse_args()
 
-    test_next_question(args.address)
-    test_submit_answer(args.address)
-    test_next_review(args.address)
-    test_submit_review(args.address)
-    test_get_labels(args.address)
+    address = args.address
+    project_id = 'de65f79e6f3391866ab4d68cbebeee1bcdc859f0'
+
+    labels = test_get_labels(address, project_id)
+
+    # Makes the the 3rd answer.
+    question = test_next_question(address, project_id)
+    test_submit_answer(address, project_id, question['id'],
+                       question['commits'][0]['id'], 'Who knows')
+    # Makes the 4th answer.
+    question = test_next_question(address, project_id)
+    test_submit_answer(address, project_id, question['id'],
+                       question['commits'][1]['id'], 'Who knows')
+
+    # Makes the 3rd review.
+    review = test_next_review(address, project_id)
+    label1 = {'commitId': review['commits'][0]['id'],
+              'labelId': labels['builtin'][0]['id']}
+    label2 = {'commitId': review['commits'][1]['id'],
+              'labelName': 'whatever'}
+    test_submit_review(address, project_id, review['id'], comment='Not known.')
+
+    # Makes the 5th and 6th answer.
+    while True:
+        question = test_next_question(address, project_id)
+        if question is None:
+            break
+        test_submit_answer(address, project_id, question['id'],
+                           question['commits'][0]['id'], 'Who knows')
+
+    # Makes the 4th - 6th reviews.
+    while True:
+        review = test_next_review(address, project_id)
+        if review is None:
+            break
+        label1 = {'commitId': review['commits'][0]['id'],
+                  'labelId': labels['builtin'][0]['id']}
+        label2 = {'commitId': review['commits'][1]['id'],
+                  'labelName': 'whatever'}
+        test_submit_review(address, project_id, review['id'],
+                           commit_labels=[label1, label2])
 
 
 if __name__ == '__main__':
