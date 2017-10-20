39e5675486953f0c04f7c0da87036beafec6766f
Wei Yonglong <w@persper.org>
2017-10-20 08:35:42 +0800

    Refactoring

--- src/components/QuestionView.vue
+++ src/components/QuestionView.vue
@@ -1,21 +1,15 @@
 <template>
   <div class="question">
-    <wireframe>
+    <wireframe :isLoading="isLoading">
       <div v-if="isCompleted">
         {{completedHint}}
       </div>
       <question-form v-else :question="questionObject" :count="count" v-on:result="fetchResult"/>
     </wireframe>
-    <div v-if="isLoading" class="loader-mask">
-      <div class="loader-wrapper">
-        <loader class="loader"></loader>
-      </div>
-    </div>
   </div>
 </template>
 
 <script>
-import ScaleLoader from 'vue-spinner/src/ScaleLoader.vue'
 import Wireframe from '@/components/Wireframe'
 import QuestionForm from '@/components/QuestionForm'
 import Storage from '@/mixins/storage'
@@ -25,7 +19,6 @@ export default {
   name: 'QuestionView',
   mixins: [Storage],
   components: {
-    'loader': ScaleLoader,
     'wireframe': Wireframe,
     'question-form': QuestionForm
   },
@@ -92,27 +85,4 @@ a {
   color: #42b983;
 }
 
-.loader-mask {
-  position: fixed;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  background: rgba(0,0,0,0.1);
-  z-index: 99;
-}
-.loader-wrapper {
-  background: rgba(255,255,255,1);
-  box-shadow: 0 0 20px rgba(0,0,0,0.2);
-  border-radius: 4px;
-  padding: 20px 0;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  width: 120px;
-  height: 40px;
-  margin-left: -60px;
-  margin-top: 50px;
-  z-index: 100;
-}
 </style>
