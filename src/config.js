6c8fa88c463de309b3c4e55349f5de6b0ce8bd03
Wei Yonglong <w@persper.org>
2017-12-05 09:11:40 +0800

    update api base

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 
 export default {
-  // API_BASE: 'http://api.persper.org/survey/v1'
-  API_BASE: 'http://127.0.0.1:5000/survey/v1'
+  API_BASE: 'http://api.persper.org/survey/v1'
+  // API_BASE: 'http://127.0.0.1:5000/survey/v1'
 }
