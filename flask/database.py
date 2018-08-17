94d8a9ddb448c5e7a45695674326ef6db0691679
Jinglei Ren <jinglei@persper.org>
2018-08-17 10:46:04 +0800

    Avoid subscription of None and use logging instead of print()

--- flask/database.py
+++ flask/database.py
@@ -1,5 +1,6 @@
 import configparser
 from hashlib import sha1
+import logging
 import secrets
 
 from neo4j.v1 import basic_auth
@@ -26,116 +27,134 @@ def init_driver():
     global _driver
     bolt, usr, pwd = neo4j_credential()
     _driver = GraphDatabase.driver(bolt, auth=basic_auth(usr, pwd))
-    print('INFO: the Neo4j driver is initialized.')
+    logging.info('The Neo4j driver is initialized.')
 
 
 def add_project(name, github_url):
     if not _driver:
         init_driver()
     pid = sha1(name.encode('utf-8')).hexdigest()
+    # noinspection PyBroadException
     try:
         project = _driver.session().write_transaction(
             query.create_project_node, pid, name, github_url)
+        if project is None:
+            return None
         assert project['id'] == pid
         if project['github_url'] != github_url:
-            print('WARN: an existing project with a different URL {name: %s} '
-                  '{url: %s} => {url: %s}' %
-                  (name, project['github_url'], github_url))
+            logging.warn('Existing project %s with a different URL: %s => %s' %
+                         (name, project['github_url'], github_url))
             return None
         return pid
-    except Exception as e:
-        print(e)
+    except Exception:
+        logging.exception("Failed to create project: " + name)
+        return None
 
 
 def get_project(project_id):
     if not _driver:
         init_driver()
+    # noinspection PyBroadException
     try:
-        return _driver.session().read_transaction(
-            query.get_project_node, project_id)
-    except Exception as e:
-        print(e)
+        return _driver.session().read_transaction(query.get_project_node,
+                                                  project_id)
+    except Exception:
+        logging.exception("Failed to retrieve info for project ID: " + project_id)
+        return None
 
 
 def add_developer(name, email):
     if not _driver:
         init_driver()
+    # noinspection PyBroadException
     try:
         token = secrets.token_urlsafe()
-        _driver.session().write_transaction(
-            query.create_developer_email, name, email, token)
+        _driver.session().write_transaction(query.create_developer_email,
+                                            name, email, token)
         return token
-    except Exception as e:
-        print(e)
+    except Exception:
+        logging.exception("Failed to create developer: %s <%s>" % (name, email))
+        return None
 
 
 def get_developer_token(email):
     if not _driver:
         init_driver()
+    # noinspection PyBroadException
     try:
-        return _driver.session().read_transaction(
-            query.get_developer_token, email)
-    except Exception as e:
-        print(e)
+        return _driver.session().read_transaction(query.get_developer_token,
+                                                  email)
+    except Exception:
+        logging.exception("Failed to get developer token: " + email)
+        return None
 
 
 def add_reviewer(email):
     if not _driver:
         init_driver()
+    # noinspection PyBroadException
     try:
         token = secrets.token_urlsafe()
-        _driver.session().write_transaction(
-            query.create_reviewer_node, email, token)
+        _driver.session().write_transaction(query.create_reviewer_node,
+                                            email, token)
         return token
-    except Exception as e:
-        print(e)
+    except Exception:
+        logging.exception("Failed to create reviewer: " + email)
+        return None
 
 
 def add_commit(*, sha1_hex, title, email, project_id):
     if not _driver:
         init_driver()
+    # noinspection PyBroadException
     try:
         tx = _driver.session().begin_transaction()
         query.create_commit_node(tx, sha1_hex, title)
         query.link_commit_author(tx, sha1_hex, email)
         query.link_project_commit(tx, sha1_hex, project_id)
         tx.commit()
-    except Exception as e:
-        print(e)
+    except Exception:
+        logging.exception("Failed to add commit: " + sha1_hex)
 
 
 def add_comparison(commit1, commit2, email):
     if not _driver:
         init_driver()
+    # noinspection PyBroadException
     try:
         cid = generate_comparison_id(commit1, commit2)
         _driver.session().write_transaction(query.create_comparison_node,
                                             cid, commit1, commit2, email)
         return cid
     except Exception as e:
-        print(e)
+        logging.exception("Failed to add comparison: %s vs. %s for %s" % (commit1, commit2, email))
+        return None
 
 
 def count_compared(token):
     if not _driver:
         init_driver()
+    # noinspection PyBroadException
     try:
-        return _driver.session().read_transaction(
-                query.count_compared_relationships, token)
-    except Exception as e:
-        print(e)
+        return _driver.session().read_transaction(query.count_compared_relationships,
+                                                  token)
+    except Exception:
+        logging.exception("Failed to count compared relationships for token: " + token)
         return -1
 
 
 def next_comparison(project_id, token):
     if not _driver:
         init_driver()
+    # noinspection PyBroadException
     try:
         comparison, commit1, commit2 = _driver.session().read_transaction(query.next_comparison_node,
                                                                           project_id, token)
+        if comparison is None:
+            return None, commit1, commit2
         return comparison['id'], commit1, commit2
-    except Exception as e:
-        print(e)
+    except Exception:
+        logging.exception("Failed to get next comparison for token: " + token)
         return None, None, None
 
 
@@ -149,19 +168,23 @@ def next_other_comparison(project_id, token, threshold):
     """
     if not _driver:
         init_driver()
+    # noinspection PyBroadException
     try:
         commit1, commit2 = _driver.session().read_transaction(query.next_other_compared_relationship,
                                                               project_id, token, threshold)
+        if commit1 is None or commit2 is None:
+            return None, commit1, commit2
         comparison_id = generate_comparison_id(commit1['id'], commit2['id'])
         return '-' + comparison_id, commit1, commit2
     except Exception as e:
-        print(e)
+        logging.exception("Failed to get next comparison authored by others for token: " + token)
         return None, None, None
 
 
 def add_answer(*, comparison_id, valuable_commit, reason, token):
     if not _driver:
         init_driver()
+    # noinspection PyBroadException
     try:
         tx = _driver.session().begin_transaction()
         if not comparison_id.startswith('-'):
@@ -174,30 +197,37 @@ def add_answer(*, comparison_id, valuable_commit, reason, token):
         assert c1 == valuable_commit
         query.create_compared_relationship(tx, comparison_id, c1, c2, reason, token)
         tx.commit()
-    except Exception as e:
-        print(e)
+    except Exception:
+        logging.exception("Failed to add answer for comparison ID: %s (token = %s)" % (comparison_id, token))
 
 
 def get_related_answers(commit, token):
     if not _driver:
         init_driver()
+    # noinspection PyBroadException
     try:
-        answers = _driver.session().read_transaction(
-            query.get_compared_relationships, commit, token)
+        answers = _driver.session().read_transaction(query.get_compared_relationships,
+                                                     commit, token)
+        if answers is None:
+            return []
         return [{'commit1': a['commit1']['id'],
                  'reason': a['outvalues']['reason'],
                  'commit2': a['commit2']['id']}
                 for a in answers]
-    except Exception as e:
-        print(e)
+    except Exception:
+        logging.exception("Failed to retrieve related answers for commit: %s (token = %s)" % (commit, token))
+        return None
 
 
 def get_related_answers_unsafe(commit):
     if not _driver:
         init_driver()
+    # noinspection PyBroadException
     try:
-        answers = _driver.session().read_transaction(
-            query.get_compared_relationships, commit)
+        answers = _driver.session().read_transaction(query.get_compared_relationships,
+                                                     commit)
+        if answers is None:
+            return []
         return [{'commit1': a['commit1']['id'],
                  'title1': a['commit1']['title'],
                  'reason': a['outvalues']['reason'],
@@ -205,117 +235,131 @@ def get_related_answers_unsafe(commit):
                  'title2': a['commit2']['title']}
                 for a in answers]
     except Exception as e:
-        print(e)
+        logging.exception("Failed to retrieve answers for commit: " + commit)
+        return None
 
 
 def next_review(project_id, token):
     if not _driver:
         init_driver()
-    n = -1
+    # noinspection PyBroadException
     try:
-        n = _driver.session().read_transaction(
-            query.count_reviewed_relationships, token)
-        compared, commit1, commit2 = _driver.session().read_transaction(
-            query.next_compared_relationship, project_id, token)
+        n = _driver.session().read_transaction(query.count_reviewed_relationships,
+                                               token)
+        compared, commit1, commit2 = _driver.session().read_transaction(query.next_compared_relationship,
+                                                                        project_id, token)
         return compared, commit1, commit2, n
-    except Exception as e:
-        print(e)
-        return None, None, None, n
+    except Exception:
+        logging.exception("Failed to fetch next review for project ID: %s (token = %s)" % (project_id, token))
+        return None, None, None, -1
 
 
 def add_label(name, genre, token):
     if not _driver:
         init_driver()
     lid = sha1(name.encode('utf-8')).hexdigest()
+    # noinspection PyBroadException
     try:
-        res_lid = _driver.session().write_transaction(
-            query.create_label_node, lid, name, genre, token)
+        res_lid = _driver.session().write_transaction(query.create_label_node,
+                                                      lid, name, genre, token)
         assert res_lid == lid
         return lid
-    except Exception as e:
-        print(e)
+    except Exception:
+        logging.exception("Failed to add label: " + name)
+        return None
 
 
 def list_labels(token):
     if not _driver:
         init_driver()
+    # noinspection PyBroadException
     try:
         rec_builtin, rec_custom = _driver.session().read_transaction(
             query.list_label_nodes, token)
         builtin = [{'id': r['label']['id'], 'name': r['label']['name']}
-                   for r in rec_builtin]
+                   for r in rec_builtin] if not rec_builtin is None else []
         custom = [{'id': r['label']['id'], 'name': r['label']['name']}
-                  for r in rec_custom]
+                  for r in rec_custom] if not rec_custom is None else []
         return builtin, custom
-    except Exception as e:
-        print(e)
+    except Exception:
+        logging.exception("Failed to list labels for token: " + token)
         return None, None
 
 
 def add_review(*, comparison_id, commit_id, label_ids, token):
     if not _driver:
         init_driver()
+    # noinspection PyBroadException
     try:
-        _driver.session().write_transaction(
-            query.create_label_relationship,
-            comparison_id, commit_id, label_ids, token)
-    except Exception as e:
-        print(e)
+        _driver.session().write_transaction(query.create_label_relationship,
+                                            comparison_id, commit_id, label_ids, token)
+    except Exception:
+        logging.exception("Failed to add review for comparison ID: %s (token = %s)" % (comparison_id, token))
 
 
 def add_comment(comparison_id, comment, token):
     if not _driver:
         init_driver()
     try:
-        _driver.session().write_transaction(
-            query.create_comment_property, comparison_id, comment, token)
-    except Exception as e:
-        print(e)
+        _driver.session().write_transaction(query.create_comment_property,
+                                            comparison_id, comment, token)
+    except Exception:
+        logging.exception("Failed to add comment for comparison ID: (token = %s)" % (comparison_id, token))
 
 
 def developer_stats(token):
     if not _driver:
         init_driver()
+    # noinspection PyBroadException
     try:
-        unanswered = _driver.session().read_transaction(
-            query.count_comparison_nodes, token)
-        answered = _driver.session().read_transaction(
-            query.count_compared_relationships, token)
+        unanswered = _driver.session().read_transaction(query.count_comparison_nodes,
+                                                        token)
+        answered = _driver.session().read_transaction(query.count_compared_relationships,
+                                                      token)
         return unanswered + answered, answered
-    except Exception as e:
-        print(e)
+    except Exception:
+        logging.exception("Failed to retrieve stats for token: " + token)
+        return None, None
 
 
 def list_email_project():
     if not _driver:
         init_driver()
+    # noinspection PyBroadException
     try:
         return _driver.session().read_transaction(query.list_email_project)
-    except Exception as e:
-        print(e)
+    except Exception:
+        logging.exception("Failed to list emails of all projects")
+        return None
 
 
 def stats_compared(project_name):
     if not _driver:
         init_driver()
+    # noinspection PyBroadException
     try:
-        counts = _driver.session().read_transaction(
-            query.list_compared_relationship_counts, project_name)
+        counts = _driver.session().read_transaction(query.list_compared_relationship_counts,
+                                                    project_name)
         return [(c['email'], c['count']) for c in counts]
-    except Exception as e:
-        print(e)
+    except Exception:
+        logging.exception("Failed to count commits compared by developers for project: " + project_name)
+        return None
 
 
 def list_compared(project_name):
     if not _driver:
         init_driver()
+    # noinspection PyBroadException
     try:
-        counts = _driver.session().read_transaction(
-            query.list_compared_relationships, project_name)
+        counts = _driver.session().read_transaction(query.list_compared_relationships,
+                                                    project_name)
+        if counts is None:
+            return []
         return [{'commit1': c['commit1']['id'],
                  'title1': c['commit1']['title'],
                  'commit2': c['commit2']['id'],
                  'title2': c['commit2']['title'],
                  'reason': c['outvalues']['reason']} for c in counts]
-    except Exception as e:
-        print(e)
+    except Exception:
+        logging.exception("Failed to list compared commits of project: " + project_name)
+        return None
