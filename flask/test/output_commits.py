7c8fe0ae5f8b5e3cd5cb3fcbdc464a37abfdea40
Jinglei Ren <jinglei@persper.org>
2018-08-19 19:00:26 +0800

    Add expected survey sequence to output_commits.py

--- flask/test/output_commits.py
+++ flask/test/output_commits.py
@@ -37,5 +37,27 @@ def main():
         json.dump(selected, f)
 
 
+# In manual test, expect the following order if the first commit is always chosen:
+# 1. yyx990803@gmail.com (#1-self): 0e53066 vs. 3d36a44
+# 2. yyx990803@gmail.com (#2-self): 0e53066 (filled) vs. 48acf71
+# 3. yyx990803@gmail.com (#3-self): 21112ec vs. 48acf71 (filled)
+# [Quit]
+# 4. 805037171@163.com (#1-self): 049f317 vs. 3a5432a
+# 5. 805037171@163.com (#2-other compared): 0e53066 vs. 3d36a44
+# 6. 805037171@163.com (#3-self): 049f317 (filled) vs. d8d4ca6
+# 7. 805037171@163.com (#4-other compared): 0e53066 (filled) vs. 48acf71
+# 8. 805037171@163.com (#5-self): 3a5432a (filled) vs. d8d4ca6 (filled)
+# 9. 805037171@163.com (#6-other compared): 21112ec vs. 48acf71 (filled)
+# 10. 805037171@163.com (#7-other comparison): 21112ec (filled) vs. 5a255d9
+# 11. 805037171@163.com (#8-other comparison): 3d36a44 (filled) vs. 5a255d9 (filled)
+# [Complete]
+# 12. yyx990803@gmail.com (#4-other compared): 049f317 vs. d8d4ca6
+# 13. yyx990803@gmail.com (#5-self): 21112ec (filled) vs. 5a255d9
+# 14. yyx990803@gmail.com (#6-other compared): 049f317 (filled) vs. 3a5432a
+# 15. yyx990803@gmail.com (#7-self): 3d36a44 (filled) vs. 5a255d9 (filled)
+# 16. yyx990803@gmail.com (#8-other compared): 3a5432a (filled) vs. d8d4ca6 (filled)
+# [Complete]
+
+
 if __name__ == '__main__':
     main()
